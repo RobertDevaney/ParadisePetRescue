4649462e7f936b7c8b3e194453fa9c70fc13a5a3
mberti4110 <134988060+mberti4110@users.noreply.github.com>
2024-03-18 21:08:37 -0400

    Updated php

--- loginform/signup_submit.php
+++ loginform/signup_submit.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$host = "localhost"; // or your Hostinger server
+$host = "localhost";
 $dbUsername = "u104394458_paradisepet";
 $dbPassword = "Petparadise2";
 $dbName = "u104394458_paradisepet";
@@ -16,14 +16,15 @@ if ($conn->connect_error) {
 // Sanitize input data
 $firstName = $conn->real_escape_string($_POST['firstname']);
 $lastName = $conn->real_escape_string($_POST['lastname']);
+$email = $conn->real_escape_string($_POST['email']);
 $phoneNumber = $conn->real_escape_string($_POST['phonenumber']);
 $username = $conn->real_escape_string($_POST['username']);
 $password = $conn->real_escape_string($_POST['password']); 
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT); // Hash the password for security
 
 // Prepare and bind
-$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, PhoneNumber, Username, Password) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $firstName, $lastName, $phoneNumber, $username, $hashedPassword);
+$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Email, PhoneNumber, Username, Password) VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("ssssss", $firstName, $lastName, $email, $phoneNumber, $username, $hashedPassword);
 
 // Execute the statement
 if ($stmt->execute()) {
@@ -32,6 +33,8 @@ if ($stmt->execute()) {
     header("Refresh:3; url=../index.html");
 } else {
     echo "Error: " . $stmt->error;
+     // Redirect to home page after 3 seconds
+    header("Refresh:3; url=../index.html");
 }
 
 $stmt->close();
